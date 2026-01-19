386f17597d920b78b46c4dc9c70a5fb020db7a76
Alexandru Lolea <alexandru.n.lolea@gmail.com>
2026-01-19 17:47:29 +0100

    ✨ Added the method definitions for ore_encrypt and secure_prf according to the paper from Stanford

--- first-interval-intersection/src/main.rs
+++ first-interval-intersection/src/main.rs
@@ -1,9 +1,27 @@
 use rand::{Rng, SeedableRng, rngs::StdRng};
 
+const M: u8 = 3;
+
+/// Generates a 128-bit secret key
 fn ore_setup() -> u128 {
     let mut rng = StdRng::from_os_rng();
 
     return rng.random();
+}
+
+/// Encrypts the 32-bit plaintext into an equally long cyphertext
+fn ore_encrypt(ore_key: u128, plaintext: u32) -> [u8; 32] {
+    let mut cyphertext = [0u8; 32];
+
+    // Go through every bit of the plaintext
+
+
+    return cyphertext;
+}
+
+/// Blinds the plaintext using AES with the ore_key as its key
+fn secure_prf(ore_key: u128, plaintext_tuple: (u8, Vec<u8>)) {
+
 }
 
 fn main() {
@@ -21,5 +39,8 @@ fn main() {
     /* =========================== STEP 1 =========================== */
     let ore_key = ore_setup();
 
-    print!("The ORE secret key is: {:?}", ore_key);
+    let plaintext_test = 42;
+    let encrypted_test = ore_encrypt(ore_key, plaintext_test);
+
+    print!("The ORE secret key is: {:?}", encrypted_test);
 }
