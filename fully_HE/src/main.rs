0392d65ca070e97e6e3c3391c7d17b773cc8b7c3
Alexandru Lolea <alexandru.n.lolea@gmail.com>
2026-02-18 19:24:33 +0100

    🎉(Re)start the FHE experiment

---
+++ fully_HE/src/main.rs
@@ -0,0 +1,8 @@
+/* ===================== IMPORTS ===================== */
+use tfhe::prelude::*;
+use tfhe::{ConfigBuilder, generate_keys, set_server_key, FheUint8};
+
+fn main() {
+    println!("Hello, world!");
+}
+
