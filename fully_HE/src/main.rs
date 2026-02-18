cc1e164f0e85ae098179911fe74865de42f019f5
Alexandru Lolea <alexandru.n.lolea@gmail.com>
2026-02-18 21:05:50 +0100

    ✨Created the PsiParty and Interval structs

--- fully_HE/src/main.rs
+++ fully_HE/src/main.rs
@@ -1,7 +1,26 @@
+use std::collections::HashSet;
+
 /* ===================== IMPORTS ===================== */
-use tfhe::prelude::*;
+use tfhe::{ClientKey, prelude::*};
 use tfhe::{ConfigBuilder, generate_keys, set_server_key, FheUint8};
 
+struct PsiParty {
+    name: String,
+    encryption_key: ClientKey,
+    intervals: HashSet<Interval>
+}
+
+impl PsiParty {
+    fn new(name: String, encryption_key: ClientKey) -> Self {
+        PsiParty { name, intervals: HashSet::new(), encryption_key }
+    }
+
+    fn add_interval(&mut self, lower: u8, upper: u8) {
+        self.intervals.insert(Interval { lower, upper });
+    }
+}
+
+#[derive(PartialEq, Eq, Hash)]
 struct Interval {
     lower: u8,
     upper: u8,
@@ -9,25 +28,26 @@ struct Interval {
 
 impl Interval {
     fn new(lower: u8, upper: u8) -> Self {
-        Interval { lower, upper } 
+        Interval { lower, upper }
     }
 }
 
 fn main() {
-    let interval= Interval::new(6, 7);
-
+    /* ======================== INITALIZE FHE SCHEMA  ======================== */
     let config = ConfigBuilder::default().build();
 
- 
+    // Create the key_pair
     let (client_key, server_key) = generate_keys(config);
+    set_server_key(server_key);
     
+    /* ======================== CREATE THE INTERVALS ======================== */
+
     let clear_a = 7u8;
     let clear_b= 128u8;
 
     let a = FheUint8::encrypt(clear_a, &client_key);
     let b = FheUint8::encrypt(clear_b, &client_key);
 
-    set_server_key(server_key);
 
     let result = a + b;
 
