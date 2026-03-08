5814aa23d007d46ae8225396308d1e38c1cba368
Alexandru Lolea <alexandru.n.lolea@gmail.com>
2026-03-08 15:48:30 +0100

    ✨ Updted the python script to create two different interval lists

--- synth_interval_gen/main.py
+++ synth_interval_gen/main.py
@@ -3,16 +3,23 @@ import random
 import json
 random.seed(42)
 
-4294967295
 # ========================= CONFIG ========================= #
-sites = random.sample(range(22, 65500, 15), 1000)
-ranges = [(i, i + random.randint(1, 14)) for i in sites]
+sites_first_interval = random.sample(range(22, 65500, 15), 1000)
+ranges_first_interval = [(i, i + random.randint(1, 14)) for i in sites_first_interval]
 
-interval_list = []
+sites_second_interval = random.sample(range(22, 65500, 10), 250)
+ranges_second_interval = [(i, i + random.randint(1, 8)) for i in sites_first_interval]
 
-for interval in ranges:
-    interval_list.append({"lower": interval[0], "upper": interval[1]})
+first_interval_list = []
+second_interval_list = []
 
+for interval in ranges_first_interval:
+    first_interval_list.append({"lower": interval[0], "upper": interval[1]})
+
+for interval in ranges_second_interval:
+    second_interval_list.append({"lower": interval[0], "upper": interval[1]})
 # 
-with open("../fully_HE/src/intervals.json", mode="w", encoding="utf-8") as write_file:
-    json.dump(interval_list, write_file)
+with open("../fully_HE/src/intervals_1.json", mode="w", encoding="utf-8") as write_file:
+    json.dump(first_interval_list, write_file)
+with open("../fully_HE/src/intervals_2.json", mode="w", encoding="utf-8") as write_file:
+    json.dump(second_interval_list, write_file)
